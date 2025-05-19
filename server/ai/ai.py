ffff36c2e2e921f8796b125f5e1934debc4fae95
clement Janusz <clement@MacBook-Air-de-clement.local>
2025-05-19 17:11:49 +0300

    Ajout AI

---
+++ server/ai/ai.py
@@ -0,0 +1,21 @@
+import openai
+from server.read_doc.read_doc import read_file
+openai.api_key = "votre-cle-api"
+
+def ask_ai(file_path, prompt):
+
+    system_prompt=""
+    user_prompt = f"{prompt} {read_file(file_path)}"
+
+    response = openai.chat.completions.create(
+        model="gpt-4o-mini",  # or "gpt-3.5-turbo" if not available
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ],
+        max_tokens=100,
+        temperature=0
+    )
+    return response.choices[0].message.content
+
+
